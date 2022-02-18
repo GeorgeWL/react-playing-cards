5df4752a2e2a58dce95f9b03495bed286ba47108
George Langham <george.langham@autodesk.com>
2022-02-18 17:47:05 +0000

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,26 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:@typescript-eslint/recommended", "plugin:storybook/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:storybook/recommended",
+    "plugin:react/jsx-runtime",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: "latest",
-    sourceType: "module"
+    sourceType: "module",
   },
   plugins: ["react", "@typescript-eslint"],
   rules: {
     semi: ["error", "always"],
-    quotes: ["error", "single"]
-  }
+    quotes: ["error", "single"],
+  },
 };
