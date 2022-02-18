a3d8ec9d71bea41f6e5f733efc73098aae3fdc14
George Langham <george.langham@autodesk.com>
2022-02-18 18:12:15 +0000

    autolinters, convert css to scss

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,37 @@
 /* eslint-disable */
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:storybook/recommended",
-    "plugin:react/jsx-runtime",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2021: true,
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["react", "@typescript-eslint"],
-  rules: {
-    semi: ["error", "always"],
-    quotes: ["error", "single"],
-  },
-  settings: {
-    react: {
-      createClass: "createReactClass", // Regex for Component Factory to use,
-      // default to "createReactClass"
-      pragma: "React", // Pragma to use, default to "React"
-      fragment: "Fragment", // Fragment to use (may be a property of <pragma>), default to "Fragment"
-      version: "detect", // React version. "detect" automatically picks the version you have installed.
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:storybook/recommended',
+        'plugin:react/jsx-runtime',
+        'prettier',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-  },
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        semi: ['error', 'always'],
+        quotes: ['error', 'single'],
+    },
+    settings: {
+        react: {
+            createClass: 'createReactClass', // Regex for Component Factory to use,
+            // default to "createReactClass"
+            pragma: 'React', // Pragma to use, default to "React"
+            fragment: 'Fragment', // Fragment to use (may be a property of <pragma>), default to "Fragment"
+            version: 'detect', // React version. "detect" automatically picks the version you have installed.
+        },
+    },
 };
